4501545c3327307949ba74f47e3e0d4e2ce6221b
Steven Mancine <stevenrmancine@gmail.com>
2021-03-25 08:11:12 -0600

    Refactor carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
 import "../../../node_modules/slick-carousel/slick/slick.css"; 
 import "../../../node_modules/slick-carousel/slick/slick-theme.css";
@@ -6,25 +6,35 @@ import './Carousel.css'
 import Hero from '../Hero/Hero.js'
 
 const Carousel = ({movies}) => {
-  const five = movies.filter((movie, index) => index < 5);
-  const featuredFilm = five.map(movie => <Hero title={movie.title} image={movie.backdrop_path} />)
-  console.log(five)
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 2000,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplaySpeed: 7000,
-      autoplay: true
-    };
-    return (
-      <div className='carousel'>
-        <Slider {...settings}>        
-            {featuredFilm}       
-        </Slider>
-      </div>
-    );
+  const featuredFilm = movies.reduce((features, movie, index) => {
+    if(index < 5) {
+      features.push(<Hero 
+        title={movie.title} 
+        image={movie.backdrop_path} 
+        key={movie.id}
+        id={movie.id}
+      />)
+    }
+    return features
+  },[]);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 7000,
+    autoplay: true
+  };
+
+  return (
+    <div className='carousel'>
+      <Slider {...settings}>        
+          {featuredFilm}       
+      </Slider>
+    </div>
+  );
   
 }
 
