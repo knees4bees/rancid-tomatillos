91db28866df6cf05851fc03bd19f535eee202c50
Katie Bee <knees4bees@gmail.com>
2021-03-25 20:33:06 -0600

    Add button elements for accessibility

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -3,9 +3,9 @@ import './Movie.css';
 
 const Movie = ({ poster, id, displayMovieDetail }) => {
   return (
-    <div className='movie' onClick={() => displayMovieDetail(id)}>
+    <button className='movie' onClick={() => displayMovieDetail(id)}>
       <img src={poster} className='movie__poster'/>
-    </div>
+    </button>
   )
 }
 
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,7 +6,9 @@ import './Nav.css';
 const Nav = () => {
   return (
     <nav className='nav-bar'>
-      <img src={homeIcon} className='nav-bar__home'/>
+      <button className='nav-bar__home' >
+        <img src={homeIcon} className='nav-bar__home--img' tabIndex='0' />
+      </button>
       <h1 className='nav-bar__header' >Rancid Tomatillos</h1>
       <img src={searchIcon} className='nav-bar__search' />
     </nav>
