4507b003724631158a434a11623e17ba6f625e3d
Katie Bee <knees4bees@gmail.com>
2021-03-25 20:11:41 -0600

    Style detail view

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './MovieDetail.css';
 
 const MovieDetail = ({details}) => {
   return (
     <div className='movie-detail'>
-      <img src={details.backdrop_path}/>
-      <h2>{details.title}</h2>
-      <p>{details.average_rating}</p>
-      <p>{details.tagline}</p>
-      <p>{details.genres}</p>
-      <p>{details.overview}</p>
-      <p>{details.runtime}</p>
-      <p>{details.release_date}</p>
+      <div className='movie-card'>
+        <img src={details.backdrop_path} className='movie-card__image' />
+        <section className='movie-card__details' >
+          <h2>{details.title}</h2>
+          <p>{details.average_rating}</p>
+          <p>{details.tagline}</p>
+          <p>{details.genres}</p>
+          <p>{details.overview}</p>
+          <p>{details.runtime}</p>
+          <p>{details.release_date}</p>
+        </section>
+      </div>
     </div>
   )
 }
