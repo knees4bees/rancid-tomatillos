d16f15f7d6eeddc7b39e371c2b613d5ae2d42a0a
Steven Mancine <stevenrmancine@gmail.com>
2021-03-26 23:44:20 -0600

    Add new fonts

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -5,14 +5,14 @@ const MovieDetail = ({details}) => {
   return (
     <div className='movie-detail'>
       <section className='movie-card__details' >
-        <img src={details.poster__path} />
-        <h2>{details.title}</h2>
-        <p>{details.average_rating}</p>
-        <p>{details.tagline}</p>
-        <p>{details.genres}</p>
-        <p>{details.overview}</p>
-        <p>{details.runtime}</p>
-        <p>{details.release_date}</p>
+        <img src={details.poster_path} className="movie-card__details__poster" />
+        <h2 className='movie-card__details__title' >{details.title}</h2>
+        <p className='movie-card__details__' >{details.average_rating}</p>
+        <p className='movie-card__details__' >{details.tagline}</p>
+        <p className='movie-card__details__' >{details.genres}</p>
+        <p className='movie-card__details__' >{details.overview}</p>
+        <p className='movie-card__details__' >{details.runtime}</p>
+        <p className='movie-card__details__' >{details.release_date}</p>
       </section>
       <div className='movie-backdrop'>
         <img className='movie-backdrop__image' src={details.backdrop_path} alt={details.title}/>
