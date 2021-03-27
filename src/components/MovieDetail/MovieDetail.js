a486bb01800ec01b52876bd791544f1d7300cb0b
Steven Mancine <stevenrmancine@gmail.com>
2021-03-27 13:09:06 -0600

    Remove unused css

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -4,7 +4,7 @@ import './MovieDetail.css';
 
 const MovieDetail = ({details}) => {
   const percentage = details.average_rating / 10 * 100
-  const computed = {
+  const ratingStyle = {
     background: `linear-gradient(to right, #ffd000 ${percentage}%, #cfe2f7d8 ${percentage}%, #d0e2f7d8 100%)`
   }
   
@@ -15,7 +15,7 @@ const MovieDetail = ({details}) => {
         <h2 className='movie-card__details__title' >{details.title}</h2>
         <h3 className='movie-card__details__date' >{details.release_date.split('-')[0]}</h3>
         <div className='movie-card__details__rating' 
-          style={computed}
+          style={ratingStyle}
         >
           <span className="fa fa-star"></span>
           <span className="fa fa-star"></span>
@@ -26,7 +26,7 @@ const MovieDetail = ({details}) => {
         <p className='movie-card__details__run-time' >{details.runtime} min</p>
         <p className='movie-card__details__genre' >{details.genres}</p>
         <p className='movie-card__details__overview' >{details.overview}</p>
-        <p className='movie-card__details__' >{details.tagline}</p>
+        <p className='movie-card__details__tagline' >{details.tagline}</p>
       </section>
       <div className='movie-backdrop'>
         <img className='movie-backdrop__image' src={details.backdrop_path} alt={details.title}/>
