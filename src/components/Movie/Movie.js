c89a717d05c781fd148c18414f6fd3ea2f8f178b
Katie Bee <knees4bees@gmail.com>
2021-03-29 16:16:28 -0600

    Add router functionality to home button

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -4,11 +4,9 @@ import { Link } from 'react-router-dom';
 
 const Movie = ({ poster, title, id, displayMovieDetail }) => {
   return (
-    // <button className="movie" onClick={() => displayMovieDetail(id)}>
-    <Link to={`/${id}`} onClick={() => displayMovieDetail(id)}>
+    <Link to={`/${id}`} className="movie" onClick={() => displayMovieDetail(id)}>
       <img className="movie__poster" src={poster} alt={title} />
     </Link>
-    // </button>
   );
 };
 
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,13 +2,14 @@ import React from 'react';
 import homeIcon from '../../home.svg';
 import searchIcon from '../../search.svg';
 import './Nav.css';
+import { Link } from 'react-router-dom';
 
 const Nav = ({ resetHome }) => {
   return (
     <nav className="nav-bar">
-      <button className="nav-bar__home" onClick={resetHome}>
+      <Link to="/" className="nav-bar__home" onClick={resetHome}>
         <img className="nav-bar__home--img" src={homeIcon} alt="home icon" />
-      </button>
+      </Link>
       <h1 className="nav-bar__header">Rancid Tomatillos</h1>
       <img className="nav-bar__search" src={searchIcon} alt="search icon" />
     </nav>
