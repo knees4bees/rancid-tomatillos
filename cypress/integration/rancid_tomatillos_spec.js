347b0cfae2fddcf860b4557da2a95f8bf4927f1e
Steven Mancine <stevenrmancine@gmail.com>
2021-03-31 17:15:57 -0600

    Add test to carousel

--- cypress/integration/rancid_tomatillos_spec.js
+++ cypress/integration/rancid_tomatillos_spec.js
@@ -1,12 +1,41 @@
 describe("Rancid Tomatillos", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+    cy.fixture('mockData').then((data) => {
+      cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 200,
+      body:  data 
+    })
+    })
+      .visit('http://localhost:3000/')
   })
 
-  it("Should see home page", () => {
+  it("Should see a navigation bar", () => {
     cy.get('nav')
       .contains('Rancid Tomatillos')
       .get('img')
       .should('have.class', 'nav-bar__search')
   });
+
+  it('Should see a carousel', () => {
+    cy.get('.slick-current')
+      .contains('The Devil')
+    // cy.get('.hero')
+    //   .contains('Tenet')
+    //   .get('.hero').get('img')
+    //   .should('have.class',  'hero__img')
+  })
+
+  it('Should be able to scroll to next image', () => {
+    cy.get('.slick-next').click()
+      .get('.slick-current')
+      .contains('Pinocchio')
+  })
+
+  it('Should be able to scroll to previous img', () => {
+    cy.get('.slick-prev').click() 
+      .get('.slick-current')
+      .contains('Face the Music')
+  })
+
+
 })
