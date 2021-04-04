be80e526078a2e84b6ff765e2be3b0d3cd66c58d
Steven Mancine <stevenrmancine@gmail.com>
2021-04-04 12:52:48 -0600

    Create Search Component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import homeIcon from '../../assets/home.svg';
 import searchIcon from '../../assets/search.svg';
+import Search from '../Search/Search';
 import './Nav.css';
 
 const Nav = ({ resetHome }) => {
@@ -11,7 +12,7 @@ const Nav = ({ resetHome }) => {
         <img className="nav-bar__home--img" src={homeIcon} alt="home icon" />
       </NavLink>
       <h1 className="nav-bar__header">Rancid Tomatillos</h1>
-      <img className="nav-bar__search" src={searchIcon} alt="search icon" />
+      <Search />
     </nav>
   );
 };
---
+++ src/components/Search/Search.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Search.css';
+import { BiSearch } from 'react-icons/bi';
+
+class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      isActive: false
+    }
+  }
+
+  render() {
+    return (
+      <div className="search">
+        
+      </div>
+    )
+  }
+}
